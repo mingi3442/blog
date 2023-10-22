f56106c936f3cd3ce467265bf2c6fd60eb34a348
Lee MinGi <mingi3442@gmail.com>
2023-10-23 00:45:44 +0900

    md, mdx test

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -78,6 +78,7 @@ function createSearchIndex(allBlogs) {
 export const Blog = defineDocumentType(() => ({
   name: 'Blog',
   filePathPattern: 'blog/**/*.{md,mdx}',
+  contentType: 'mdx',
   fields: {
     title: { type: 'string', required: true },
     date: { type: 'date', required: true },
