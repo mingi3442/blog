0f8fee7418489285bca435e34e5d58b81aab77b7
Lee MinGi <mingi3442@gmail.com>
2023-10-29 19:32:34 +0900

    Edit Tag CSS, Post CSS

--- app/Main.tsx
+++ app/Main.tsx
@@ -4,8 +4,9 @@ import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
 import Image from 'next/image'
+import BlogCard from '@/components/BlogCard'
 
-const MAX_DISPLAY = 5
+const MAX_DISPLAY = 6
 
 export default function Home({ posts }) {
   return (
@@ -19,59 +20,18 @@ export default function Home({ posts }) {
             {siteMetadata.description}
           </p>
         </div>
-        {/* <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            테스트
-          </h1>
-        </div> */}
+
+        {/* <ul className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-2 "> */}
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((post) => {
             const { slug, date, title, summary, tags, images } = post
-
             return (
-              <li key={slug} className="py-12">
-                <article>
-                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                    <div className="space-y-5 xl:col-span-3">
-                      <div className="space-y-6">
-                        <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                            <Link
-                              href={`/blog/${slug}`}
-                              className="text-gray-900 dark:text-gray-100"
-                            >
-                              {title}
-                            </Link>
-                          </h2>
-                          <div className="flex flex-wrap">
-                            {tags.map((tag) => (
-                              <Tag key={tag} text={tag} />
-                            ))}
-                          </div>
-                        </div>
-                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                          {summary}
-                        </div>
-                      </div>
-                      <div className="text-base font-medium leading-6">
-                        <Link
-                          href={`/blog/${slug}`}
-                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                          aria-label={`Read "${title}"`}
-                        >
-                          Read more &rarr;
-                        </Link>
-                      </div>
-                      <dl>
-                        <dt className="sr-only">Published on</dt>
-                        <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                          <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
-                        </dd>
-                      </dl>
-                    </div>
-                  </div>
-                </article>
+              <li
+                key={slug}
+                className="divide-y divide-gray-200 dark:divide-gray-700 py-8  w-[100%] flex items-center justify-center"
+              >
+                <BlogCard post={post} />
               </li>
             )
           })}
--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -1,47 +1,88 @@
-export default function BlogCard() {
+import Link from 'next/link'
+import siteMetadata from '@/data/siteMetadata'
+import { formatDate } from 'pliny/utils/formatDate'
+import Tag from '@/components/Tag'
+import Image from 'next/image'
+export default function BlogCard({
+  post,
+}: {
+  post: {
+    slug: string
+    date: string
+    title: string
+    summary: string
+    tags: Array<string>
+    images: Array<string>
+  }
+}) {
+  const { slug, date, title, summary, tags, images } = post
   return (
-    <div className="blog-card spring-fever relative w-350 h-500 mx-auto rounded-lg shadow-md text-center">
-      <div className="title-content absolute z-20 w-full top-0 left-0 mt-70 text-center">
-        <h3 className="text-2xl font-medium tracking-wide text-blue-400 mb-0">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        </h3>
-        <div className="w-20 h-3 my-5 mx-auto border-0 bg-yellow-400" />
-        <div className="intro w-44 mx-auto text-gray-300 font-serif text-sm italic leading-5">
-          Yllamco,laboris, nisi ut aliquip ex ea commodo.
+    <div className="container">
+      <div className="lg:-mx-6 lg:flex lg:items-center">
+        <div className="relative  overflow-hidden bg-clip-border w-full lg:mx-6 lg:w-1/2 rounded-xl h-72 lg:h-92 ">
+          <Image
+            className="w-full lg:w-1/2 rounded-xl h-72 lg:h-96"
+            sizes="100vw"
+            src={Array.isArray(images) ? images[0] : ''}
+            alt=""
+            fill
+            style={{ objectFit: 'cover' }}
+          />
+        </div>
+        <div className="mt-2 lg:w-1/2 lg:mt-0 flex flex-col items-start justify-start h-full">
+          <p className="block text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">
+            {title}
+          </p>
+          <div className="mt-4 flex flex-wrap">
+            {tags.map((tag) => (
+              <Tag key={tag} text={tag} />
+            ))}
+          </div>
+
+          <p className="mt-3 text-sm text-gray-500 dark:text-gray-300 md:text-sm">{summary}</p>
+          <button className="mt-2 btn group flex items-center bg-transparent tracking-widest text-base font-medium leading-6">
+            <span className="relative pb-1 pr-2 text-white after:transition-transform after:duration-500 after:ease-out after:absolute after:bottom-0 after:left-0 after:block after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-primary-500 after:content-[''] after:group-hover:origin-bottom-left after:group-hover:scale-x-100 ">
+              <Link
+                href={`/blog/${slug}`}
+                className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                aria-label={`Read "${title}"`}
+              >
+                Read more
+              </Link>{' '}
+            </span>
+            <svg
+              viewBox="0 0 46 16"
+              height="8"
+              width="12"
+              xmlns="http://www.w3.org/2000/svg"
+              id="arrow-horizontal"
+              className="-translate-x-2 fill-slate-700 transition-all duration-300 ease-out group-hover:translate-x-1 group-hover:scale-x-105 group-hover:fill-white"
+            >
+              <path
+                transform="translate(30)"
+                d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z"
+                data-name="Path 10"
+                id="Path_10"
+              ></path>
+            </svg>
+          </button>
+          <div className="flex items-center mt-6">
+            <div className="object-cover object-center w-10 h-10 rounded-full bg-slate-600"></div>
+
+            <div className="mx-4">
+              <h1 className="text-sm text-gray-700 dark:text-gray-200">Amelia. Anderson</h1>
+              <p className="text-sm text-gray-500 dark:text-gray-400">Lead Developer</p>
+            </div>
+          </div>
+
+          <dl>
+            <dt className="sr-only">Published on</dt>
+            <dd className="text-sm font-medium leading-6 text-gray-500 dark:text-gray-400">
+              <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
+            </dd>
+          </dl>
         </div>
       </div>
-      <div className="card-info absolute z-20 w-full bottom-10 left-0 mx-auto px-16 text-gray-300 font-serif text-sm leading-6 opacity-0 transition-transform duration-300">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Ut enim ad minim.
-      </div>
-      <div className="utility-info absolute z-20 bottom-0 left-0">
-        <ul className="utility-list list-none m-0 ml-20 pb-30 w-full">
-          <li className="comments inline-block mr-15">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-4 h-4 inline-block -mt-1 mr-1 fill-current text-gray-300"
-              viewBox="0 0 24 24"
-            >
-              <path d="M0 0h24v24H0z" fill="none" />
-              <path d="M19 21H5c-1.11 0-2-.89-2-2V5c0-1.11.89-2 2-2h6v2H5v14h14v-6h2v6c0 1.11-.89 2-2 2zm-2-12H7v6h10v-6zm0-4H7v2h2z" />
-            </svg>
-            12
-          </li>
-          <li className="date inline-block">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-4 h-4 inline-block -mt-1 mr-1 fill-current text-gray-300"
-              viewBox="0 0 24 24"
-            >
-              <path d="M0 0h24v24H0z" fill="none" />
-              <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2zm0-8h-2v2h2z" />
-            </svg>
-            03.12.2015
-          </li>
-        </ul>
-      </div>
-      <div className="gradient-overlay absolute w-350 h-500 bottom-0 left-0 z-15 bg-gradient-to-b from-transparent to-[rgba(0,0,0,0.6)]" />
-      <div className="color-overlay absolute w-350 h-500 top-0 left-0 z-10 bg-[rgba(84,104,110,0.4)] transition-background duration-300" />
     </div>
   )
 }
