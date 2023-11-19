8ef1921583e02c32f6f44d9481aa3280d111d4bd
Lee MinGi <mingi3442@gmail.com>
2023-11-20 02:42:09 +0900

    naver verification

--- app/head.tsx
+++ app/head.tsx
@@ -11,6 +11,7 @@ export default function Head() {
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="naver-site-verification" content="e81e6e126603dd692215c6a310f910212f74c463" />
     </>
   )
 }
