95000b41afbbc035c405e89890df37a8ca867ee3
Lee MinGi <mingi3442@gmail.com>
2024-12-04 21:16:04 +0900

    ⏪️ Sitemap.ts 재수정
    Signed-off-by: Lee MinGi <mingi3442@gmail.com>

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -14,30 +14,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
     lastModified: new Date().toISOString().split('T')[0],
   }))
 
-  const allRoutes = [...routes, ...blogRoutes]
-
-  // Sitemap 객체를 생성
-  const sitemapEntries = allRoutes.map((route) => ({
-    url: route.url,
-    lastModified: route.lastModified,
-  }))
-
-  // XML 문자열 생성
-  const xml = `
-<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${sitemapEntries
-    .map(
-      (entry) => `
-    <url>
-      <loc>${entry.url}</loc>
-      <lastmod>${entry.lastModified}</lastmod>
-    </url>
-  `
-    )
-    .join('')}
-</urlset>
-  `
-
-  return xml.trim() as unknown as MetadataRoute.Sitemap // XML 문자열을 Sitemap 타입으로 변환
+  return [...routes, ...blogRoutes]
 }
