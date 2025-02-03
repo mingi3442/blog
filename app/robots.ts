e4ecfa60db409ae382e0f412782e2a6b05ef47c4
Lee MinGi <mingi3442@gmail.com>
2025-02-03 11:29:05 +0900

    🔧 Update sitemap.ts, robots.ts
    Signed-off-by: Lee MinGi <mingi3442@gmail.com>

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,15 @@
 import siteMetadata from '@/data/siteMetadata'
 import { MetadataRoute } from 'next'
 
+export const dynamic = 'force-static'
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: ['/'],
-      disallow: [],
+      allow: '/',
     },
     sitemap: `${siteMetadata.siteUrl}/sitemap.xml`,
+    host: siteMetadata.siteUrl,
   }
 }
