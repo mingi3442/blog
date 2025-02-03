6c88987c345b146236270f6fd6591703ed626d8d
Lee MinGi <mingi3442@gmail.com>
2025-02-03 11:01:41 +0900

    🔧 Update robots.ts
    Signed-off-by: Lee MinGi <mingi3442@gmail.com>

--- app/robots.ts
+++ app/robots.ts
@@ -2,16 +2,11 @@ import siteMetadata from '@/data/siteMetadata'
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const siteUrl = siteMetadata.siteUrl
-
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-      },
-    ],
-    host: siteUrl,
-    sitemap: `${siteUrl}/sitemap.xml`,
+    rules: {
+      userAgent: '*',
+      disallow: [],
+    },
+    sitemap: `${siteMetadata.siteUrl}/sitemap.xml`,
   }
 }
