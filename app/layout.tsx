59f19cd21a0e71e9c698389273bf030112466416
이민기 <mingi3442@gmail.com>
2025-07-16 14:22:11 +0900

    :lock: update CSP headers to allow Google fonts and analytics services

--- app/layout.tsx
+++ app/layout.tsx
@@ -99,10 +99,6 @@ export default function RootLayout({
         <link rel="manifest" href="/static/favicons/site.webmanifest" />
         <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#000000" />
-        <meta
-          httpEquiv="Content-Security-Policy"
-          content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' *.google.com *.googletagmanager.com *.gstatic.com giscus.app analytics.umami.is https://va.vercel-scripts.com *.vercel-insights.com; connect-src 'self' *.google-analytics.com *.googletagmanager.com *.gstatic.com vitals.vercel-insights.com vercel.com *.vercel-insights.com; img-src 'self' data: *.google.com *.googleusercontent.com; style-src 'self' 'unsafe-inline' *.googleapis.com; font-src 'self' *.gstatic.com *.googleapis.com; frame-src giscus.app;"
-        />
         <meta name="theme-color" media="(prefers-color-scheme: light)" content="#f1f1f1" />
         <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
         <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
--- next.config.js
+++ next.config.js
@@ -7,13 +7,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is *.vercel-insights.com *.googletagmanager.com *.google-analytics.com;
-  style-src 'self' 'unsafe-inline';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is *.vercel-insights.com *.googletagmanager.com *.google-analytics.com *.google.com va.vercel-scripts.com;
+  style-src 'self' 'unsafe-inline' *.googleapis.com;
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
-  connect-src 'self' *.google-analytics.com *.googletagmanager.com *.gstatic.com vitals.vercel-insights.com vercel.com *.vercel-insights.com;
-  font-src 'self';
-  frame-src giscus.app
+  connect-src 'self' *.google-analytics.com *.googletagmanager.com *.gstatic.com vitals.vercel-insights.com vercel.com *.vercel-insights.com *.vercel.app;
+  font-src 'self' *.gstatic.com *.googleapis.com;
+  frame-src giscus.app *.google.com
 `
 
 const securityHeaders = [
